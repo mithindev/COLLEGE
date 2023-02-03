297039a14ee64cbd1cf08be614ccfb3737043b16
mithindev <mithindev1@gmail.com>
2023-02-03 09:38:53 +0530

    updated

---
+++ MATLAB/CODE/Qn.m
@@ -0,0 +1,13 @@
+%Q1
+clc; clear; close all;
+A = randi([0 9],3);
+
+%%
+%Q2
+clc; clear; close all;
+
+
+%%
+%Q3
+clc; clear; close all;
+B = randi([0 9],3,1)*randi([0 9],1, 3);
--- MATLAB/PRACTICE/SET-2/clock.m
+++ MATLAB/PRACTICE/SET-2/clock.m
@@ -7,5 +7,5 @@ i = 1;
 
 while time180(i) < timeend
     i = i+1;
-    time180(i) = time180(i-1)+ (180*11)/2
+    time180(i) = time180(i-1)+ 180*11/2
 end
