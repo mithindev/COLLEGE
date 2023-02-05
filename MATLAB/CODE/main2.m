3f52ebbe0e462132c52e0365960b8d2dc7f6a0ee
mithindev <mithindev1@gmail.com>
2023-02-05 17:54:30 +0530

    updated

--- MATLAB/CODE/main2.m
+++ MATLAB/CODE/main2.m
@@ -1,11 +1,8 @@
-clc; clear; close all;
-
 A = randi([0 9], 5, 2)*randi([0 9], 2, 5);
-B = A*randi([0 9], 5, 1);
+B = randi([0 9], 5, 1);
 Xp = pinv(A)*B;
 Nsv = null(A);
 nCol = size(Nsv, 2);
 Xn = Nsv*randi([0, 9], nCol, 1);
 X = Xp+Xn;
 disp(X)
-
