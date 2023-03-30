5a19d10625f0a73de58e7816bf42b5c7e430df0d
mithindev1 <mithindev1@gmail.com>
2023-03-30 11:48:54 +0530

    updated

---
+++ SEM-2/JAVA/src/com/mithin/LINKED_LIST/Double_Lin_List.java
@@ -0,0 +1,84 @@
+package com.mithin.LINKED_LIST;
+
+public class Double_Lin_List {
+    private Node head;
+
+    public void insertFirst (int val) {
+        Node node = new Node(val);
+
+        node.next = head;
+        node.prev = null;
+        if (head != null) {
+            head.prev = node;
+        }
+        head = node;
+    }
+
+    public void insertLast (int val) {
+        Node node = new Node(val);
+        Node temp = head;
+
+        if (head == null) {
+            node.prev = null;
+            node.next = null;
+            head = node;
+            return;
+        }
+
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+
+        temp.next = node;
+        node.next = null;
+        node.prev = temp;
+    }
+
+    public void insert (int val, int index) {
+        Node node = new Node(val);
+        if (index == 1) {
+            insertFirst(val);
+        }
+    }
+    public void display() {
+        Node node = head;
+        Node last = null;
+
+        while (node != null) {
+            System.out.print(node.value + " -> ");
+            last = node;
+            node = node.next;
+        }
+        System.out.println("END");
+
+        while (last != null) {
+            System.out.print(last.value + " -> ");
+            last = last.prev;
+        }
+        System.out.println("START");
+    }
+
+    public Node get (int index) {
+        Node temp = head;
+        for (int i = 0; i < index; i++) {
+            temp = temp.next;
+        }
+        return temp;
+    }
+
+    private class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node (int val) {
+            this.value = val;
+        }
+
+        Node (int val, Node next, Node prev) {
+            this.value = val;
+            this.next = next;
+            this.prev= prev;
+        }
+    }
+}
--- SEM-2/JAVA/src/com/mithin/LINKED_LIST/Main.java
+++ SEM-2/JAVA/src/com/mithin/LINKED_LIST/Main.java
@@ -2,11 +2,17 @@ package com.mithin.LINKED_LIST;
 
 public class Main {
     public static void main(String[] args) {
-
+/*
         LinList LL = new LinList();
         LL.insertFirst(3);
         LL.insertFirst(4);
 
         LL.display();
+ */
+        Double_Lin_List DLl = new Double_Lin_List();
+        DLl.insertFirst(1);
+        DLl.insertFirst(2);
+        DLl.insertFirst(3);
+        DLl.display();
     }
 }
