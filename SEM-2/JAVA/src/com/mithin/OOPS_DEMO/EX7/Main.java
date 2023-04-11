1bac02e9ee4067ef455fa4e2ed46cd48ede16ed0
mithindev1 <mithindev1@gmail.com>
2023-04-11 17:57:12 +0530

    updated

---
+++ SEM-2/JAVA/src/com/mithin/OOPS_DEMO/EX7/Main.java
@@ -0,0 +1,13 @@
+package com.mithin.OOPS_DEMO.EX7;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator casio = new Calculator(5, 0);
+
+        casio.add();
+        casio.div();
+
+    }
+
+}
