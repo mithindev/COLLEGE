e2af3084fbbcf7ebe0a9006d5badf0c288137fc4
mithindev1 <mithindev1@gmail.com>
2023-05-08 09:44:39 +0530

    updated

---
+++ SEM-2/JAVA/src/com/mithin/OOPS_DEMO/SET2/Main.java
@@ -0,0 +1,17 @@
+package com.mithin.OOPS_DEMO.SET2;
+
+public class Main {
+    public static void main(String[] args) {
+        Test o1 = new Test();
+        Test.add();
+        Test o2 = new Test();
+        Test.add();
+
+        System.out.println(Test.count);
+    }
+
+    /*
+     Note:
+     If the variable is static the just ust `class.var_name`
+    */
+}
---
+++ SEM-2/JAVA/src/com/mithin/OOPS_DEMO/SET2/Test.java
@@ -0,0 +1,13 @@
+package com.mithin.OOPS_DEMO.SET2;
+
+class Test {
+    static int count = 0;
+
+    Test() {
+        count++;
+    }
+
+    static void add() {
+        count += 100;
+    }
+}
