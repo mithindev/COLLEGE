abc71f0fa9967d6f5956da97289d67795a2cddf9
mithindev <mithindev1@gmail.com>
2025-01-03 11:49:47 +0530

    updated

--- BIG DATA/hello-scala/dev.scala
+++ BIG DATA/hello-scala/dev.scala
@@ -1,5 +1,40 @@
-object Dev {
-    def main(args: Array[String]): Unit = {
-        println("Hello, World!")
-    }
+object Scala_List_Methods {
+  def main(args: Array[String]): Unit = {
+    // Define two lists
+    val x = List(1, 40, 10)
+    val y = List(20, 25, 30)
+    val z = List("DataCamp", "Tutorials", "Are", "The", "Best")
+
+    println()
+    println("Head of list x, y, z = " + (x.head, y.head, z.head))
+    println()
+
+    println()
+    println("Tail of list x, y, z = " + (x.tail, y.tail, z.tail))
+    println()
+
+    println()
+    println("Is list x, y, z empty? = " + (x.isEmpty, y.isEmpty, z.isEmpty))
+    println()
+
+
+    println()
+    println("Concatenate x and y = " + (x ++ y))
+    println()
+
+    // Reverse
+    println()
+    println("Reverse of x, y, z is = " + (x.reverse, y.reverse, z.reverse))
+    println()
+
+    // Fill
+    println()
+    println("Output of Fill method is = " + List.fill(5)(z))
+    println()
+
+    // Tabulate
+    println()
+    println("Output of Tabulate method is = " + List.tabulate(5)(n => n * n * n))
+    println()
+  }
 }
